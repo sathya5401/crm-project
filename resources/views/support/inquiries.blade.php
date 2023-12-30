305cbda762888a3178e04b3a70fb0f0906457cca
ainkayrul <nrlain1812@gmail.com>
2023-12-31 01:20:45 +0800

    latest change

--- resources/views/support/inquiries.blade.php
+++ resources/views/support/inquiries.blade.php
@@ -1,9 +1,6 @@
 <!-- resources/views/inquiry/create.blade.php -->
 <form method="POST" action="{{ url('/inquiries) }}">
     @csrf
-    <input type="text" id="id" placeholder="Your ID">
-    <input type="text" name="name" placeholder="Your Name">
-    <input type="email" name="email" placeholder="Your Email">
     <textarea name="message" placeholder="Your Message"></textarea>
     <button type="submit">Submit Inquiry</button>
 </form>
