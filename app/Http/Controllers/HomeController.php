3dfd221e0b3436412bce2786786bd50a20576039
sathya5401 <sathyarao5401@gmail.com>
2024-01-09 16:38:11 +0800

    update homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User; 
 use App\Models\Task;
 use App\Models\Rfx;
-
+use App\Models\Meeting;
 
 
 class HomeController extends Controller
@@ -35,7 +35,20 @@ class HomeController extends Controller
         $rfx = Rfx::where('user_id', $user->id)
                     ->whereIn('Status',['new', 'in-progress',]) 
                     ->get();
+        
+        // Fetch upcoming meetings
+        $upcomingMeetings = Meeting::where(function ($query) use ($user) {
+            // Include meetings where the authenticated user is a participant
+            $query->whereHas('participants', function ($innerQuery) use ($user) {
+                $innerQuery->where('user_id', $user->id);
+            })
+            // Include meetings where the authenticated user is the host
+            ->orWhere('host_id', $user->id);
+        })
+        ->where('to', '>=', now())
+        ->orderBy('from')
+        ->get();
 
-        return view('home', ['user' => $user, 'tasks' => $tasks, 'rfx' => $rfx]);
+        return view('home', ['user' => $user, 'tasks' => $tasks, 'rfx' => $rfx, 'upcomingMeetings' => $upcomingMeetings,]);
     }
 }
